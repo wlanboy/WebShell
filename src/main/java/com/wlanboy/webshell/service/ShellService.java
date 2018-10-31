6e02e1e48897cc8bfe639ccdab6a533916264afe
wlanboy <me@wlanboy.com>
2018-10-31 19:50:54 +0100

    Added Shellservice for persistent shell.

---
+++ src/main/java/com/wlanboy/webshell/service/ShellService.java
@@ -0,0 +1,36 @@
+package com.wlanboy.webshell.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShellService {
+
+	private Runtime runtime;
+	private Process p;
+	
+	public String callCommand(String command) throws IOException, InterruptedException {
+		String output = null;
+		
+		StringBuilder buffer = new StringBuilder();
+
+		if (runtime == null) {
+			runtime = Runtime.getRuntime();
+		}
+		p = runtime.exec(command);
+		p.waitFor();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+		String line = "";
+		while ((line = reader.readLine()) != null) {
+			buffer.append(line + "\n");
+		}
+
+		output = buffer.toString();
+		
+		return output;
+	}
+}
