2c9d149e4a7703d61d515d9ac498f716b57d6604
wlanboy <me@wlanboy.com>
2018-10-31 19:50:08 +0100

    Added pom.xml and Application.

---
+++ src/main/java/com/wlanboy/webshell/WebshellApplication.java
@@ -0,0 +1,12 @@
+package com.wlanboy.webshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshellApplication.class, args);
+	}
+}
