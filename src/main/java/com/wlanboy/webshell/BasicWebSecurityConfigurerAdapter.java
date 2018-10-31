875b02f09d6a7a056b9bf7980c684d2b982dcccc
wlanboy <me@wlanboy.com>
2018-10-31 20:38:52 +0100

    Added encoded passwords

--- src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
+++ src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
@@ -1,24 +1,29 @@
 package com.wlanboy.webshell;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class BasicWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+	@Value("${userpassword}")
+	String userpassword;
+	@Value("${testpassword}")
+	String testpassword;
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/test", "/webjars").permitAll()
+                .antMatchers("/test", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -26,19 +31,22 @@ public class BasicWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdap
                 .permitAll()
                 .and()
             .logout()
-                .permitAll();
+                .permitAll()
+                .and()
+                .httpBasic();
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("SHELL")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder())
+            .withUser("user").password(userpassword).roles("SHELL")
+            .and()
+            .withUser("test").password(testpassword).roles("SHELL");
+    }    
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }    
 }
