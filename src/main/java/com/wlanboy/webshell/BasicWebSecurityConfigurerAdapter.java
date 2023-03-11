b336461b7534627387cdc39a364654dfdbc83a12
wlanboy <me@wlanboy.com>
2023-03-11 10:10:15 +0100

    switched to SecurityFilterChain

--- src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
+++ src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
@@ -3,50 +3,50 @@ package com.wlanboy.webshell;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class BasicWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+public class BasicWebSecurityConfigurerAdapter {
 
 	@Value("${userpassword}")
 	String userpassword;
 	@Value("${testpassword}")
 	String testpassword;
+    String role = "SHELL";
 	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/test", "/webjars/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll()
-                .and()
-                .httpBasic();
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+      http
+        .authorizeHttpRequests(requests -> requests
+        .requestMatchers( new AntPathRequestMatcher("/webjars/**")).permitAll()
+        .requestMatchers( new AntPathRequestMatcher("swagger-ui/**")).permitAll()
+        .requestMatchers( new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+        .requestMatchers( new AntPathRequestMatcher("v3/api-docs/**")).permitAll()
+        .requestMatchers( new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+        .anyRequest().authenticated())
+        .formLogin(form -> form.loginPage("/login").permitAll());
+      return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .passwordEncoder(passwordEncoder())
-            .withUser("user").password(userpassword).roles("SHELL")
-            .and()
-            .withUser("test").password(testpassword).roles("SHELL");
-    }    
-    
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }    
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+            .password(userpassword)
+            .roles(role)
+            .build();
+        UserDetails test = User.withUsername("test")
+            .password(testpassword)
+            .roles(role)
+            .build();
+        return new InMemoryUserDetailsManager(user,test);
+    }
 }
