8480eaba73422689d9fec486f6a7289104d84943
wlanboy <wlanboy@github.com>
2025-11-29 15:23:06 +0100

    Moved to Lambda

--- src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
+++ src/main/java/com/wlanboy/webshell/BasicWebSecurityConfigurerAdapter.java
@@ -9,42 +9,39 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class BasicWebSecurityConfigurerAdapter {
 
-	@Value("${userpassword}")
-	String userpassword;
-	@Value("${testpassword}")
-	String testpassword;
-    String role = "SHELL";
-	
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-      http
-        .authorizeHttpRequests(requests -> requests
-        .requestMatchers( new AntPathRequestMatcher("/webjars/**")).permitAll()
-        .requestMatchers( new AntPathRequestMatcher("swagger-ui/**")).permitAll()
-        .requestMatchers( new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
-        .requestMatchers( new AntPathRequestMatcher("v3/api-docs/**")).permitAll()
-        .requestMatchers( new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
-        .anyRequest().authenticated())
-        .formLogin(form -> form.loginPage("/login").permitAll());
-      return http.build();
-    }
+  @Value("${userpassword}")
+  String userpassword;
+  @Value("${testpassword}")
+  String testpassword;
+  String role = "SHELL";
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withUsername("user")
-            .password(userpassword)
-            .roles(role)
-            .build();
-        UserDetails test = User.withUsername("test")
-            .password(testpassword)
-            .roles(role)
-            .build();
-        return new InMemoryUserDetailsManager(user,test);
-    }
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(requests -> requests
+            .requestMatchers("/webjars/**").permitAll()
+            .requestMatchers("/swagger-ui/**").permitAll()
+            .requestMatchers("/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated())
+        .formLogin(form -> form.loginPage("/login").permitAll());
+    return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withUsername("user")
+        .password(userpassword)
+        .roles(role)
+        .build();
+    UserDetails test = User.withUsername("test")
+        .password(testpassword)
+        .roles(role)
+        .build();
+    return new InMemoryUserDetailsManager(user, test);
+  }
 }
